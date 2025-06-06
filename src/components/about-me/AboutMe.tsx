97770b944f69aa3328d558c8f80da565e9f1249b
eltyagi <lakshya_tyagi@icloud.com>
2025-06-06 12:56:10 +0530

    feat: Add chat-based contact form and enhance site pages

--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import './AboutMe.css';
 import logoImg from '../../assets/music.svg'; // Adjust the path as necessary
+import profileImg from '../../assets/myimg.png'; // Profile image
 import Navigation from '../navigation/Navigation';
+import PageHeader from '../page-header/PageHeader';
 
 const AboutMe: React.FC = () => {
   return (
     <div className="about-me">
-      <div className = 'pg-title'>
-            <div className = 'pg-title-name krona-one-regular'>
-                eltyagi's
-            </div>
-            <div className='pg-title-icon'>
-                <img className = 'pg-title-icon-img' src={logoImg} alt="Logo" />
-            </div>
-            <div className = 'pg-title-subtitle krona-one-regular'>
-                dock
-            </div>
+     <div className = 'pg-title-container'>
+            <PageHeader/>
         </div>
 
-      <div className='about-me-content fira-code-regular'>
+      <div className='about-me-main-content'>
+        <div className='about-me-image'>
+          <img src={profileImg} alt="Profile" className='profile-img' />
+        </div>
+        
+        <div className='about-me-text fira-code-regular'>
+          Hi there, I'm Lakshya and welcome to my digital home!<br/><br/>
+          I am a Solutions Engineer, <a href='https://github.com/eltyagi'>GitHub</a>. By day, I help CTOs, 
+          Engineering Managers and VPs drive engineering productivity. 
+          By night, I am building, creating and teaching and more..
+          There is much to cover, so much to learn so I am happy to have you here.
+        </div>
       </div>
 
       <div className='nav'>

811ecc7ef03feb0e3dffa49f0b81f5e93adfd4d5
eltyagi <lakshya_tyagi@icloud.com>
2026-02-19 11:04:42 +1100

    Updates

---
+++ assets/post5-iSWn4suX.js
@@ -0,0 +1,26 @@
+const e=`---
+title: Escaping the AI Rhetoric
+classification: Journal
+excerpt: As an SE, you're not just keeping yourself up to speed with the latest in the industry, but also with the rhetoric which drives perception in the minds of developers and decision makers. 
+date: 2026-02-19
+---
+
+As a Solutions Engineer in the developer tooling and AI space, a lot of what I do on a day to day basis is have conversations with engineering leaders, founders, developers about, well, AI. Even though there is lot to offer, everything from actions to application security, AI continues to dominate. Both in terms of our product vision, but also in the minds of our customers. 
+
+As an SE, you're not just keeping yourself up to speed with the latest in the industry, but also with the rhetoric which drives perception in the minds of developers and decision makers. 
+
+Every new post on Linkedin or X, which states "<insert product here> is dead", somehow shapes the perception of the leaders and engineers I speak to, no matter how bad these takes might be. This is new for me, technology has historically been objective. Sure, there have always been debates on framework hell and language preferences but, never have I seen tweets and youtube videos define how tech is adopted in large organizations working on sensitive applications.
+
+Well, in the world of developer tools, this is the new reality. First, terms like "Vibe coding" are invented, preached as the gospel truth, and then shot down by the same preachers. Influencers will influence, but these takes are now picked up by engineering leaders and founders as guiding principles in how they build and deploy products. As an engineer, and an advisor to my customers, I am not a big fan of this cycle.
+
+But I digress.
+
+Over the past week, I have been in the process of immigrating to Sydney. Immigration, ofc, comes with many real world challenges. Eating through my savings, finding a safe home, learning the local culture, and the constant inner conflict on the RoI of such a move, are one of many things that I'm sure every immigrant goes through.
+
+But in my case, this has been uniquely helpful. A break, from the endless rhetoric that has gripped the tech industry today. For the past week, I have not concerned myself with our changelog, or how it sits in comparison to that of OpenAI or Anthropic. For once, I have not paid attention to the latest "hot-takes" on tech. I have not been asked to explain how AI will magically refactor decades old code. And it has been refreshing. A reminder, that most of the world is largely unaffected by the so-called "impending doom" that is AI. 
+
+A reminder to self that banking, government services, real-estate, immigration, aviation, defence and many other industries exist today, which continue to solve fundamental problems. A reminder that as an SE, developing an instinct around how users interact with these industries, and how organizations build for these industries continues to be a priority, more so, than the hot-takes and apparently dead frameworks that send tech influencers into a frenzy. 
+
+
+
+`;export{e as default};
